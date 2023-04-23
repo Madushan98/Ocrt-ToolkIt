b0ce1a6ca0e138381504fcac0b3f6fdc287b7ec8
madushan <ranasinhemadushan@gmail.com>
2023-04-24 02:11:57 +0530

    main ocr processes added

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ import pytesseract
 from pytesseract import Output
 import easyocr
 import cv2
+from internal.ocr_process import easyOcr_reader, easyOcr_text_only
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -22,7 +23,6 @@ if os.path.isdir("result"):
     pass
 else:
     os.mkdir("result")
-
 
 
 @app.get("/")
@@ -43,42 +43,59 @@ async def root():
     return {"message": "Wellcome to OcRT API"}
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+@app.post("/get-result")
+async def getResult(file: UploadFile = File(...)):
     if os.path.isdir(uploaded_path):
         pass
     else:
         os.mkdir(uploaded_path)
-    filename = f"image/{file.filename}"
+    filename = f"uploads/{file.filename}"
     with open(filename, "wb") as f:
         shutil.copyfileobj(file.file, f)
 
     # output from tesseract and easyocr
     op_tesseract = pytesseract.image_to_string(filename)
-    op_easyocr = easyocr.Reader(["en"]).readtext(filename, paragraph=True)
+    op_easyocr = easyOcr_text_only(filename)
 
-    # Getting bounding boxes from easyocr
-    cord_ocr = op_easyocr[-1][0]
-    x_min, y_min = [min(idx) for idx in zip(*cord_ocr)]  # min cord
-    x_max, y_max = [max(idx) for idx in zip(*cord_ocr)]  # max cord
+    # # Getting bounding boxes from easyocr
+    # cord_ocr = op_easyocr[-1][0]
+    # x_min, y_min = [min(idx) for idx in zip(*cord_ocr)]  # min cord
+    # x_max, y_max = [max(idx) for idx in zip(*cord_ocr)]  # max cord
 
-    img_ocr = cv2.imread(filename)
-    cv2.rectangle(img_ocr, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2) # get the bounding box on image
-    cv2.imwrite("result/easyocr.png", img_ocr) # save the image
+    # img_ocr = cv2.imread(filename)
+    # cv2.rectangle(img_ocr, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2) # get the bounding box on image
+    # cv2.imwrite("result/easyocr.png", img_ocr) # save the image
 
-    # Getting bounding boxes from tesseract
-    img_tesseract = cv2.imread(filename)  # read image
-    d = pytesseract.image_to_data(Image.open(filename), output_type=Output.DICT) # dict-form boundary
-    n_boxes = len(d['level']) # find the text box
+    # # Getting bounding boxes from tesseract
+    # img_tesseract = cv2.imread(filename)  # read image
+    # d = pytesseract.image_to_data(Image.open(filename), output_type=Output.DICT) # dict-form boundary
+    # n_boxes = len(d['level']) # find the text box
 
-    for i in range(n_boxes):
-        (x, y, w, h) = (d['left'][i], d['top'][i], 
-                        d['width'][i], d['height'][i])
-        cv2.rectangle(img_tesseract, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # for i in range(n_boxes):
+    #     (x, y, w, h) = (d['left'][i], d['top'][i],
+    #                     d['width'][i], d['height'][i])
+    #     cv2.rectangle(img_tesseract, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imwrite('result/tesseract.png', img_tesseract)
+    # cv2.imwrite('result/tesseract.png', img_tesseract)
 
     return {
         "Tesseract": op_tesseract,
-        "EasyOcr": op_easyocr[-1][-1]
+        "EasyOcr": op_easyocr
     }
+
+
+@app.post("/upload")
+async def uploadImage(file: UploadFile = File(...)):
+    if os.path.isdir(uploaded_path):
+        pass
+    else:
+        os.mkdir(uploaded_path)
+    filename = f"uploads/upload.{file.filename.split('.')[-1]}"
+    with open(filename, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+
+@app.post("/pre-process")
+async def preProcessImage():
+    # todo: add preprocess image
+    pass
