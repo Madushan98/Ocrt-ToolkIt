1ae9d0c4c07b5c5569590d16cc1efeeff6fd44e0
PREMINDA <premindabandara0777@gmail.com>
2023-04-28 17:41:35 +0530

    add text area

--- frontend/components/filePreview.tsx
+++ frontend/components/filePreview.tsx
@@ -1,13 +1,17 @@
 'use client'
 import { useState, useContext, useEffect } from 'react'
 import { FileContext } from '../app/context'
+import FileUpload from './fileUpload'
 
 
 const FilePreview = () => {
     const fileContext = useContext(FileContext)
     const [selectedFile, setSelectedFile] =  useState<Blob | null>(null)
     const [hovered, setHovered] = useState(false)
+    const [selectimage, setSelectImage] = useState<File|null>(null)
     const [isDragging, setIsDragging] = useState(false);
+    const [option, setOption] = useState<string>("option1");
+    const [textAreResult,setTextAreaResult] = useState<string>("asdasd");
 
     const handleHover = () => {
         setHovered(true)
@@ -33,20 +37,58 @@ const FilePreview = () => {
         const files = e.dataTransfer.files;
         if (files.length > 0) {
             const file = files[0];
-            setSelectedFile(selectedFile);
-            console.log(URL.createObjectURL(file));
-            fileContext?.setPreviewUrl(URL.createObjectURL(file));
+            setFile(file)
+            
         }
     };
 
+    const handleFileInput = (event: React.ChangeEvent<HTMLInputElement> ) => {
+        const file = event.target.files?.[0]
+        if (file) {
+            setFile(file)
+            setSelectImage(file)
+        }
+       
+      }
+
+    const setFile=(file : File)=>{
+        setSelectedFile(selectedFile);
+        console.log(URL.createObjectURL(file));
+        fileContext?.setPreviewUrl(URL.createObjectURL(file));
+    }
+
+    const handleOptionSelect=(value:string)=>{
+        setOption(value);
+    }
+
+    const apiCall= async ()=>{
+        console.log("call")
+        if(!selectimage)return;   
+        const formData = new FormData();
+        const boundary = Math.random().toString().slice(2);
+        formData.append('file', selectimage);
+        const options = {
+                            method: 'POST',
+                            body: formData,
+                        };
+
+        const result = await fetch('http://localhost:8000/get-result', options)
+                        .then((response) => response.json())
+                        
+        setTextAreaResult(result.EasyOcr)
+
+    }
+
     return (
+        <>
+        <FileUpload onUpload={()=>apiCall()} onOptionChange={handleOptionSelect} />
         <div
             className={`relative overflow-hidden ${hovered ? 'shadow-xl' : 'shadow-md'} mt-8`}
             onMouseEnter={handleHover}
             onMouseLeave={handleLeave}
         >
             <div className="flex bg-gray-100 rounded-lg">
-                <div  className="w-1/2 border-r p-4">
+                <div  className="w-1/2  border-r p-4">
                     {fileContext?.previewUrl ? (
                         // eslint-disable-next-line @next/next/no-img-element
                         <img
@@ -59,30 +101,44 @@ const FilePreview = () => {
                         onDragOver={handleDragOver}
                         onDrop={handleDrop}
                         onDragLeave={handleDragLeave}
-                        className={`rounded-lg z-10 h-48 w-full flex justify-center items-center ${isDragging?'border-2':'border'} border-dashed border-blue-500 `}>
-                            <span className="text-gray-500 ">Select a file or Drag and Drop</span>
+                        className={`relative rounded-lg z-10 h-48 w-full flex justify-center items-center ${isDragging?'border-2':'border'} border-dashed border-blue-500 `}>
+                            <span className="text-gray-500 ">Click on to Select a File or Drag and Drop</span>
+                            <input onChange={handleFileInput} type="file"  className="absolute opacity-0 inset-0 w-full h-full  cursor-pointer" />
                         </div>
                     )}
                 </div>
                 <div className="w-1/2 p-4">
-                {fileContext?.previewUrl ? (
-                        // eslint-disable-next-line @next/next/no-img-element
-                        <img
-                            className="object-cover"
-                            src={fileContext?.previewUrl}
-                            alt="File Preview"
-                        />
-                    ) : (
-                        <div className="h-48 w-full bg-gray-100 flex justify-center items-center">
-                            <span className="text-gray-500">No file selected</span>
+                {option == 'get-result'?
+                    (
+                        <textarea value={textAreResult} readOnly className='w-full h-full rounded-lg p-1'>
+                        </textarea>
+                    ):
+                    (
+                        <div className="h-48 w-full bg-gray-100 flex justify-center items-center focus:border-transparent">
+                            <span className="text-gray-500">Upload File to Get Results</span>
                         </div>
-                    )}
+                    )
+                }
                 </div>
             </div>
 
         </div>
+        </>
     )
 }
 
 
 export default FilePreview
+
+// {fileContext?.previewUrl ? (
+//     // eslint-disable-next-line @next/next/no-img-element
+//     <img
+//         className="object-cover"
+//         src={fileContext?.previewUrl}
+//         alt="File Preview"
+//     />
+// ) : (
+//     <div className="h-48 w-full bg-gray-100 flex justify-center items-center">
+//         <span className="text-gray-500">Upload File to Get Results</span>
+//     </div>
+// )}
