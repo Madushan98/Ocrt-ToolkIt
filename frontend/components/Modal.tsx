b8e6ceedbff4ff5b395beb964f3030af45179fd8
PREMINDA <premindabandara0777@gmail.com>
2023-04-28 18:40:24 +0530

    refactor modal

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -9,14 +9,7 @@ export default function Modal({children}:any) {
               <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
            </div>   
            <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>          
-           <div className="inline-block align-bottom bg-secondary rounded-lg text-left overflow-hidden
-              shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="py-4 text-sky-50 bg-slate-700 text-left px-6">
-                  <div className="flex-col pb-4">
-                  {children}
-                  </div>
-              </div> 
-          </div>
+           {children}
           </div>
       </div>
       
