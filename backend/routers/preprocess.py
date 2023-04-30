118f92641a5a6df737cb57d5dedf4d9ac976ce15
madushan <ranasinhemadushan@gmail.com>
2023-04-30 22:00:43 +0530

    frontend: Options enum added

--- backend/routers/preprocess.py
+++ backend/routers/preprocess.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import os
 from internal.noise_remove import noise_remove_total
+from internal.dilate_erode import dilate_total, erote_total
 from pathlib import Path
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -23,10 +24,17 @@ save_path = os.path.join(os.path.dirname(os.getcwd()),
 @router.get("/pre-process/noise-remove")
 async def removeNoise():
     noise_remove_total(file_path, save_path, th1=210, th2=230)
-    # image_url = FileResponse(save_path)
-    # return {"image_url": image_url}
-
     url = f"http://localhost:8000/static/results/{resultFlename}"
 
     # Return a JSON response with the image URL
     return JSONResponse({"url": url})
+
+
+@router.get("/pre-process/noise-remove")
+async def dilation():
+    dilate_total(file_path, save_path)
+    url = f"http://localhost:8000/static/results/{resultFlename}"
+
+    # Return a JSON response with the image URL
+    return JSONResponse({"url": url})
+    
